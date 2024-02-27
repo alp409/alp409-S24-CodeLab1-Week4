31f16f3f69385f2f75daf77d5e155104046e0225
Amanda <157532117+alp409@users.noreply.github.com>
2024-02-27 11:41:19 -0500

    Fixed high score bug
    Updated homework, fixed high score bug.

--- CodeLab1-S24-Week4-MoreDataHomework/Assets/Scripts/GameManager.cs
+++ CodeLab1-S24-Week4-MoreDataHomework/Assets/Scripts/GameManager.cs
@@ -35,46 +35,10 @@ public class GameManager : MonoBehaviour
         set
         {
             score = value;
-            //if (isInGame == false)                            // TODO: GET THIS TO WORK 
-            //{
-            //Debug.Log("isInGame = false");
-                if (isHighScore(score))  
-                {
-                    Debug.Log("isHighScore check was true");
-                    int highScoreSlot = -1;                     // default to -1 because ????
-
-                    for (int i = 0; i < HighScores.Count; i++)  // forloop to go through the HighScores List
-                    {
-                        if (score > highScores[i])              // if it finds a score that is higher than a highscore
-                        {                                       // in a certain slot
-                            
-                            highScoreSlot = i;                  // this tells you where you want to insert the new value
-                            break;                              // and breaks (ends) the forloop
-                        }
-                    }
-
-                    highScores.Insert(highScoreSlot, score);       // inserts the score into the highScoreSlot 
-                                                                            // the highScoreSlot was found in the foreloop above
-                    highScores = highScores.GetRange(0, 5);       // this limits the size of the highScoresList to 5 slots
-
-                    string scoreBoardText = "";                             // turns the ints into a string
-
-                    foreach (var highScore in highScores)                // foreach reads the whole highScores list in order
-                    {
-                        scoreBoardText += highScore + "\n";                 // \n puts each score on it's own line
-                    }
-
-                    highScoresString = scoreBoardText;                      // string to hold the scoreBoardText
-
-                    File.WriteAllText(FILE_FULL_PATH, highScoresString);
-                }
-            //}
         }
-
     }
 
     string highScoresString = "";                                       // declaring a string to hold the scoreBoardText (used above)
-    
     List<int> highScores;                                               // a LIST of high scores, holds several pieces of data
 
     public List<int> HighScores                                         // a property to wrap around HighScore
@@ -83,6 +47,7 @@ public class GameManager : MonoBehaviour
         {
             if (highScores == null)                                     // if nothing has been put into the list yet (return empty list to highScores)
             {
+                Debug.Log("got from file");
                 highScores = new List<int>();                           // 
                 highScoresString = File.ReadAllText(FILE_FULL_PATH);    // get the contents of the data file (a string)
 
@@ -98,19 +63,22 @@ public class GameManager : MonoBehaviour
                     int currentScore = Int32.Parse(highScoreArray[i]);  // Int32 used to convert the strings to ints
                     highScores.Add(currentScore);                       // insert into our high score list
                 }
+            }  else if(highScores == null)
+            {
+                Debug.Log("NOPE");
+                highScores = new List<int>();
+                highScores.Add(3);
+                highScores.Add(2);
+                highScores.Add(1);
+                highScores.Add(0);
             }
 
             return highScores;
         }
-    }
-
-    //float timer = 0;
-    //public int maxTime = 10;
-    //bool isInGame = true;              
+    }      
     
     void Awake()                        // happens before the scene runs (before start)
-                                        // makes game manager into a singleton across scenes
-    {
+    {                                   // makes game manager into a singleton across scenes
         if (instance == null)
         {
             instance = this;
@@ -134,10 +102,12 @@ public class GameManager : MonoBehaviour
     {
         if (isInGame)                           // while game is running, set display to show score and time
         {
+            //Debug.Log("Is in game bool: TRUE");
             display.text = "Score: " + score + "\nTime: " + (maxTime - (int)timer);
         }
         else                                    // change display text for end scene to show score and GAME OVER
         {
+            //Debug.Log("Is in game bool: FALSE");
             display.text = "GAME OVER\nFINAL SCORE: " + score + "\nHigh Scores:\n" + highScoresString;   // list of high scores
         }
         
@@ -153,6 +123,7 @@ public class GameManager : MonoBehaviour
             isInGame = false;                   // destroy any prizes with tag "prize"
             SceneManager.LoadScene("EndScene");
             Destroy(GameObject.FindWithTag("prize"));
+            SetHighScore();
         }
 
         timer += Time.deltaTime;
@@ -160,7 +131,6 @@ public class GameManager : MonoBehaviour
     
     bool isHighScore(int score)                     // TODO: how do i make this only run at the end?
     {
-        //Debug.Log("checking -- isHighScore? BOOL");
         for (int i = 0; i < HighScores.Count; i++)  // go through the high scores list to see if the score
         {                                           // is higher, the Count is how many slots are in the list
             if (highScores[i] < score)              // if the highscore is less than the current score
@@ -170,5 +140,38 @@ public class GameManager : MonoBehaviour
         }
 
         return false;                               // if not, return false, ie the score is not a high score
+    }
+
+    void SetHighScore()
+    {
+        if (isHighScore(score))
+        {
+            Debug.Log("isHighScore check was true");
+            int highScoreSlot = -1;                     // default to -1 because ????
+
+            for (int i = 0; i < HighScores.Count; i++)  // forloop to go through the HighScores List
+            {
+                if (score > highScores[i])              // if it finds a score that is higher than a highscore
+                {                                       // in a certain slot
+                    highScoreSlot = i;                  // this tells you where you want to insert the new value
+                    break;                              // and breaks (ends) the forloop
+                }
+            }
+
+            highScores.Insert(highScoreSlot, score);    // inserts the score into the highScoreSlot 
+                                                                 // the highScoreSlot was found in the foreloop above
+            highScores = highScores.GetRange(0, 5);    // this limits the size of the highScoresList to 5 slots
+
+            string scoreBoardText = "";                          // turns the ints into a string
+
+            foreach (var highScore in highScores)             // foreach reads the whole highScores list in order
+            {
+                scoreBoardText += highScore + "\n";              // \n puts each score on it's own line
+            }
+
+            highScoresString = scoreBoardText;                   // string to hold the scoreBoardText
+
+            File.WriteAllText(FILE_FULL_PATH, highScoresString);
+        }
     }
 }
